86b4c3e8e98dcee25d774f9c39287e3ad73da3c3
Michael Doppler <76885460+mdop@users.noreply.github.com>
2025-11-03 13:12:30 +0100

    Add material spectrum  input processing and tests

---
+++ src/spectrophane/io/material_spectrum_processing.py
@@ -0,0 +1,82 @@
+import json
+from typing import Dict, Tuple, Sequence
+from numbers import Number
+import numpy as np
+from dataclasses import dataclass
+
+from spectrophane.io.data_io import get_resource_path
+from spectrophane.io.stack_io import stack_json_to_array, StackData
+
+@dataclass
+class TrainingRefSpectraData:
+    transmission_stacks: StackData
+    transmission_spectra: np.ndarray
+    reflection_stacks: StackData
+    reflection_spectra: np.ndarray
+    min_wavelength: Number
+    step_wavelength: Number
+    fallback_spectrumlength: Number
+
+
+def import_measured_spectra(filename: str) -> Tuple[Dict, Sequence]:
+    """Imports provided spectra provided in the source data required for parsing spectral data. Returns spectra and the material list for stack analysis"""
+    total_path = get_resource_path("material_data/" + filename)
+    with open(total_path, "r") as file:
+        data = json.load(file)
+    return data.get("spectra", {}), data.get("materials", {})
+
+def get_common_wavelength_space(min_wavelengths: Sequence[Number], step_wavelengths: Sequence[Number], spectrum_lengths: Sequence[int]) -> Tuple[Number, Number, int] | Tuple[None, None, None]:
+    """Takes a set of spectra and finds a suitable common wavelength space for analysis expressed as (min_wavelength, step_wavelength, values)"""
+    if len(min_wavelengths) != len(step_wavelengths) or len(min_wavelengths) != len(spectrum_lengths):
+        raise IndexError(f"get_common_wavelength_space input values do not have the same length. min_wavelength len {len(min_wavelengths)}, step_wavelength len {len(step_wavelengths)}, spectrum_lengths len {len(spectrum_lengths)}")
+    end_wavelengths = [0] * len(min_wavelengths)
+    for i in range(len(min_wavelengths)):
+        end_wavelengths[i] = min_wavelengths[i] + (spectrum_lengths[i] - 1) * step_wavelengths[i]
+    #clip wavelength range and choose smallest step size
+    com_min = max(min_wavelengths)
+    com_step = min(step_wavelengths)
+    com_end = min(end_wavelengths)
+    com_length = int((com_end-com_min)/com_step) + 1
+    return com_min, com_step, com_length
+
+def reshape_spectrum(old_min_wavelength: Number, old_step_wavelength: Number, old_values: Sequence[Number], new_min_wavelength: Number, new_step_wavelength: Number, new_length: int) -> np.ndarray:
+    """Takes a raw spectrum as presented in the input data file and interpolates it to fit a different wavelength space. Uses linear interpolation"""
+    old_end_wavelength = old_min_wavelength+old_step_wavelength*(len(old_values)-1)
+    wavelengths_old = np.linspace(old_min_wavelength, old_end_wavelength, num=len(old_values))
+    new_end_wavelength = new_min_wavelength+new_step_wavelength*(new_length-1)
+    wavelengths_new = np.linspace(new_min_wavelength, new_end_wavelength, num=new_length)
+    return np.interp(wavelengths_new, wavelengths_old, old_values)
+
+def process_spectrum_list(spectrum_data_list: Sequence[Dict], materials: Sequence[Dict], min_wavelength: Number, step_wavelength: Number, spectrum_length: int) -> Tuple[StackData, np.ndarray] | Tuple[None, None]:
+    """Takes data from a list of spectrum data from the source file and returns output grade stack and spectrum data. If given an empty spectrum list will return Null, Null"""
+    if len(spectrum_data_list) == 0:
+        return None, None
+    output_spectra = np.zeros((len(spectrum_data_list), spectrum_length),dtype=np.float64)
+    stack_data_list = [entry["stack"] for entry in spectrum_data_list]
+    stack_output = stack_json_to_array(materials, stack_data_list)
+    for i, spectrum_dict in enumerate(spectrum_data_list):
+        spectrum_output = reshape_spectrum(spectrum_dict["wl_start"], spectrum_dict["wl_step"], spectrum_dict["value"], min_wavelength, step_wavelength, spectrum_length)
+        output_spectra[i, :] = spectrum_output
+    return stack_output, output_spectra
+
+def prepare_spectrum_data(filename: str = "default.json") -> TrainingRefSpectraData | None:
+    """Takes the raw data filename and parses data, transforms spectra to common denominator, and returns a harmonized spectral dataset and associated stacks. If no data are found returns None"""
+    spectra_dict, materials = import_measured_spectra(filename)
+
+    #find common wavelength space
+    min_wavelengths = []
+    step_wavelengths = []
+    spectrum_lengths = []
+    for spectrum_type in spectra_dict.values():
+        for spectrum_entry in spectrum_type:
+            min_wavelengths.append(spectrum_entry["wl_start"])
+            step_wavelengths.append(spectrum_entry["wl_step"])
+            spectrum_lengths.append(len(spectrum_entry["value"]))
+    if len(min_wavelengths) == 0:
+        return None
+    com_min_wavelength, com_step_wavelength, com_spectrum_length = get_common_wavelength_space(min_wavelengths, step_wavelengths, spectrum_lengths)
+
+    #transform and compile spectrum data
+    transmission_stacks, transmission_spectra = process_spectrum_list(spectra_dict.get("transmission", {}), materials, com_min_wavelength, com_step_wavelength, com_spectrum_length)
+    reflection_stacks, reflection_spectra = process_spectrum_list(spectra_dict.get("reflection", {}), materials, com_min_wavelength, com_step_wavelength, com_spectrum_length)
+    return TrainingRefSpectraData(transmission_stacks, transmission_spectra, reflection_stacks, reflection_spectra, com_min_wavelength, com_step_wavelength, com_spectrum_length)
---
+++ tests/io/test_material_spectrum_processing.py
@@ -0,0 +1,178 @@
+import pytest
+import numpy as np
+import json
+import copy
+from unittest.mock import ANY
+from spectrophane.io.material_spectrum_processing import (
+        import_measured_spectra,
+        get_common_wavelength_space,
+        reshape_spectrum,
+        process_spectrum_list,
+        prepare_spectrum_data,
+        TrainingRefSpectraData
+)
+from spectrophane.io.stack_io import StackData
+
+@pytest.fixture
+def mock_empty_source_file():
+    data = {
+        "materials": [
+            {
+            "id": "bl_jwhite",
+            },
+            {
+            "id": "bl_yellow",
+            }
+        ],
+
+        "spectra": { }
+    }
+    return data
+
+@pytest.fixture
+def mock_transmission_source_file(mock_empty_source_file):
+    data = copy.deepcopy(mock_empty_source_file)
+    data["spectra"] = {
+        "transmission": [
+            {
+                "wl_start": 380,
+                "wl_step": 1,
+                "stack": [{"id": "bl_jwhite", "d":0.20}],
+                "value": [1] * 200
+            },
+            {
+                "wl_start": 400,
+                "wl_step": 2,
+                "stack": [{"id": "bl_jwhite", "d":0.20}, {"id": "bl_yellow", "d":0.10}, {"id": "bl_jwhite", "d":0.30}, {"id": "bl_yellow", "d":0.40}],
+                "value": [1] * 110
+            },
+            {
+                "wl_start": 380,
+                "wl_step": 2,
+                "stack": [{"id": "bl_jwhite", "d":0.20}],
+                "value": [1] * 100
+            },
+            {
+                "wl_start": 410,
+                "wl_step": 2,
+                "stack": [{"id": "bl_jwhite", "d":0.20}],
+                "value": [1] * 100
+            },
+            {
+                "wl_start": 410,
+                "wl_step": 2,
+                "stack": [{"id": "bl_jwhite", "d":0.20}],
+                "value": [1] * 90
+            }
+        ]
+    }
+    return data
+
+@pytest.fixture
+def mock_reflection_source_file(mock_empty_source_file):
+    data = copy.deepcopy(mock_empty_source_file)
+    data["spectra"] = {
+        "reflection": [
+            {
+                "wl_start": 380,
+                "wl_step": 1,
+                "stack": [{"id": "bl_jwhite", "d":0.20}],
+                "value": [1] * 200
+            },
+            {
+                "wl_start": 400,
+                "wl_step": 2,
+                "stack": [{"id": "bl_jwhite", "d":0.20}],
+                "value": [1] * 110
+            }
+        ]
+    }
+    return data
+
+@pytest.fixture
+def mock_combined_source_file(mock_transmission_source_file, mock_reflection_source_file):
+    data = copy.deepcopy(mock_transmission_source_file)
+    data["spectra"]["reflection"] = mock_reflection_source_file["spectra"]["reflection"]
+    return data
+
+@pytest.mark.parametrize(("data","spectra_out","materials_out"),
+                         [({"spectra": "Spectra", "materials": "Materials", "dummy": "somedata"}, "Spectra", "Materials"),
+                          ({"dummy": "somedata"}, {}, {}),
+                          ({"spectra": "Spectra", "dummy": "somedata"}, "Spectra", {}),
+                          ({"materials": "Materials", "dummy": "somedata"}, {}, "Materials")])
+def test_import_measured_spectra(mocker, data, spectra_out, materials_out):
+    mock_file = mocker.mock_open(read_data=json.dumps(data))
+    mocker.patch('builtins.open', mock_file)
+    
+    result_spectra, result_materials = import_measured_spectra("test.json")
+
+    assert result_spectra == spectra_out
+    assert result_materials == materials_out
+
+@pytest.mark.parametrize(("min", "step", "lengths", "ref_min", "ref_step", "ref_length"), [
+                            ((0,0,0,0),(1,1,1,1),(10,20,30,40),0,1,10),
+                            ((0,3,2,1),(1,1,1,1),(10,10,10,10),3,1,7),
+                            ((0,0,0,0),(4,2,3,1),(10,10,10,10),0,1,10),
+                            ((100,90,110,105),(1,2,3,1),(30,20,40,50),110,1,19),
+                            ((100,90,110,105),(5,2,10,5),(11,100,10,50),110,2,21)
+                        ])
+def test_get_common_wavelength_space(min, step, lengths, ref_min, ref_step, ref_length):
+    res_min, res_step, res_length = get_common_wavelength_space(min, step, lengths)
+    assert res_min == ref_min
+    assert res_step == ref_step
+    assert res_length == ref_length
+
+@pytest.mark.parametrize(("old_min_wavelength, old_step_wavelength, old_values, new_min_wavelength, new_step_wavelength, new_length, new_values"), [
+                            (0,1,[1,2,3,4,5,6,7,8,9,10], 4,2,3,[5,7,9]),
+                            (0,2,[1,2,3,4,5,6,7,8,9,10], 2,2,4,[2,3,4,5])
+                        ])
+def test_reshape_spectrum(old_min_wavelength, old_step_wavelength, old_values, new_min_wavelength, new_step_wavelength, new_length, new_values):
+    result = reshape_spectrum(old_min_wavelength, old_step_wavelength, old_values, new_min_wavelength, new_step_wavelength, new_length)
+    new_values_arr = np.array(new_values)
+    assert (result == new_values_arr).all()
+
+def test_process_spectrum_list(mock_empty_source_file, mock_transmission_source_file):
+    empty_result = process_spectrum_list(mock_empty_source_file["spectra"].get("transmission", {},), mock_empty_source_file.get("materials", {}), None, None, None)
+    assert empty_result == (None, None)
+
+    transmission_result = process_spectrum_list(mock_transmission_source_file["spectra"].get("transmission", {},), mock_transmission_source_file.get("materials", {}), 410, 1, 80)
+    assert isinstance(transmission_result[0], StackData)
+    assert transmission_result[1].shape == (5,80)
+
+def test_prepare_spectrum_data_empty(mocker, mock_empty_source_file):
+    mock_empty = mocker.mock_open(read_data=json.dumps(mock_empty_source_file))
+    mocker.patch('builtins.open', mock_empty)
+    empty_result = prepare_spectrum_data()
+    assert empty_result is None
+
+def test_prepare_spectrum_data_transmission(mocker, mock_transmission_source_file):
+    mock_transmission = mocker.mock_open(read_data=json.dumps(mock_transmission_source_file))
+    mocker.patch('builtins.open', mock_transmission)
+    transmission_result = prepare_spectrum_data()
+    assert isinstance(transmission_result, TrainingRefSpectraData)
+    assert transmission_result.reflection_spectra is None
+    assert transmission_result.reflection_stacks is None
+    assert isinstance(transmission_result.transmission_stacks, StackData)
+    assert isinstance(transmission_result.transmission_spectra, np.ndarray)
+
+def test_prepare_spectrum_data_reflection(mocker, mock_reflection_source_file):
+    mock_reflection = mocker.mock_open(read_data=json.dumps(mock_reflection_source_file))
+    mocker.patch('builtins.open', mock_reflection)
+    reflection_result = prepare_spectrum_data()
+    assert isinstance(reflection_result, TrainingRefSpectraData)
+    assert reflection_result.transmission_spectra is None
+    assert reflection_result.transmission_stacks is None
+    assert isinstance(reflection_result.reflection_stacks, StackData)
+    assert isinstance(reflection_result.reflection_spectra, np.ndarray)
+
+def test_prepare_spectrum_data_combined(mocker, mock_combined_source_file):
+    mock_combined = mocker.mock_open(read_data=json.dumps(mock_combined_source_file))
+    mocker.patch('builtins.open', mock_combined)
+    combined_result = prepare_spectrum_data()
+    assert isinstance(combined_result, TrainingRefSpectraData)
+    assert isinstance(combined_result.transmission_stacks, StackData)
+    assert isinstance(combined_result.transmission_spectra, np.ndarray)
+    assert isinstance(combined_result.reflection_stacks, StackData)
+    assert isinstance(combined_result.reflection_spectra, np.ndarray)
+    assert combined_result.transmission_spectra.shape == (5,ANY)
+    assert combined_result.reflection_spectra.shape == (2,ANY)
