Add normalization to spectrum to xyz formula and correct to achieve correct calculation of D65/2° observer in new test.

src/spectrophane/core/color_transformations.py
@@ -23,13 +23,28 @@ def linrgb_to_xyz(rgb_values: np.ndarray, matrix: str|np.ndarray = "sRGB", clip:
         xyz = np.clip(xyz, 0, 1)
     return xyz
 
-def spectrum_to_xyz(material_spectrum: np.ndarray | jnp.ndarray, light_source: np.ndarray | jnp.ndarray, observer: np.ndarray | jnp.ndarray, step_wavelength: Number):
-    """Takes illuminator spectrum (shape (N,)), relative intensity spectrum (e.g. transmission or reflection spectrum, shape (N,)), and an observer (e.g. CIE1931, shape (3,N)) and integrates to XYZ color space."""
+def compute_spectrum_xyz_normalization_factor(light_source: np.ndarray | jnp.ndarray, observer: np.ndarray | jnp.ndarray, step_wavelength: Number):
+    """Calculates normalization factor for spectrum to xyz conversion for indirect illumination from harmonized spectra of the light source and observer"""
+    if isinstance(light_source, np.ndarray):
+        MD = np
+    else:
+        MD = jnp
+    factor = MD.trapezoid(light_source * observer[1], None, step_wavelength)
+    print(factor)
+    return factor
+    
+
+def spectrum_to_xyz(material_spectrum: np.ndarray | jnp.ndarray, light_source: np.ndarray | jnp.ndarray, observer: np.ndarray | jnp.ndarray, step_wavelength: Number, normalization_factor: Number | None = None):
+    """Takes illuminator spectrum (shape (N,)), relative intensity spectrum (e.g. transmission or reflection spectrum, shape (N,)), and an observer (e.g. CIE1931, shape (3,N)) and integrates to XYZ color space.
+        pre-calculated normalization_factor may be passed for calculation speedup"""
     if isinstance(material_spectrum, np.ndarray):
         MD = np
     else:
         MD = jnp
     detector_spectrum = material_spectrum * light_source
     excitation_spectrum = detector_spectrum[MD.newaxis, :] * observer
-    xyz = MD.trapezoid(excitation_spectrum, None, dx=step_wavelength, axis=1).flatten()
+    if normalization_factor is None:
+        normalization_factor = compute_spectrum_xyz_normalization_factor(light_source, observer, step_wavelength)
+    print(excitation_spectrum)
+    xyz = MD.trapezoid(excitation_spectrum, None, dx=step_wavelength, axis=1).flatten() / normalization_factor
     return xyz