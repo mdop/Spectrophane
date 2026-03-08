69c353265f0c9e277aa62e5da860f5fe635cb062
Michael Doppler <76885460+mdop@users.noreply.github.com>
2026-03-08 13:37:05 +0100

    Add sample script for lithophane generation

---
+++ example_scripts/lithophane_creation.py
@@ -0,0 +1,26 @@
+from typer.testing import CliRunner
+from pathlib import Path
+
+from spectrophane.application.cli.main import app
+
+runner = CliRunner()
+image_path = Path(__file__).parent / "test_image.jpg"
+output_base_path = Path.home() / "Spectrophane" / "test_output" / "test"
+output_base_path.parent.mkdir(parents=True, exist_ok=True)
+
+result = runner.invoke(
+    app,
+    [
+        "lithophane",
+        "--image-path", str(image_path),
+        "--output-base", str(output_base_path),
+        "--layer-count", 8,
+        "--layer-thickness", 0.03,
+        "--top-thickness", 0.03,
+        "--resolution", 100,150,
+        "--pixel-size", 0.8,0.8,
+    ],
+)
+print(result.exit_code)
+print(result.stdout)
+print(result.stderr)
